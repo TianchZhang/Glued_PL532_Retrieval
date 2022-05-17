8a3aa78c4a06461d390bbf07631ce7a808659130
Tianch Zhang <solar.ztc@gmail.com>
2022-05-17 10:12:25 +0800

    Revert "update"
    This reverts commit 3426a1ee9e4e3b441fd9e95ce011ba1bea440926.

--- plicel_main.m
+++ plicel_main.m
@@ -1,8 +1,8 @@
 
     for mon=1:12
         for da = 1:31
-            if exist(['E:\DATA\PLidar532\Raw_Data\DATA','2019','\CH1\',num2str(mon,'%02d'),num2str(da,'%02d')],'dir')
-                batch_licelGlue(datenum(2019,mon,da),'PLidar',false,'MUA_PLidar_Config.m');
+            if exist(['E:\DATA\PLidar532\DATA','2020','\CH1\',num2str(mon,'%02d'),num2str(da,'%02d')],'dir')
+                batch_licelGlue(datenum(2020,mon,da),'PLidar',false,'MUA_PLidar_Config.m');
             end
         end
     end
